57179e9db61c0a97ab012b198aedef6669869d57
Askar Holte Storbakk <askst050900@troms.vgs.no>
2020-09-02 11:39:32 +0200

    added some stuff

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+console.log('Initializing...');
+
+// Load required library
+const fs = require('fs');
+
+// Load config file
+const {prefix, token} = require('config.json');
+
+// Initialize discord api
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// Add collection of commands to client (bot)
+client.commands = new Discord.Collection();
+
+// Runs when bot is ready!
+client.on('ready', () => {
+  console.log('Bot is ready...');
+});
+
+// Listen for messages
+client.on('message', message => {
+
+});
+
+// Connect bot to account with token
+client.login(token);
