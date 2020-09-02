9dfb77de6eb657eb381a1673928af723133c185e
Askar Holte Storbakk <askst050900@troms.vgs.no>
2020-09-02 13:57:23 +0200

    Added command handler

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const {prefix, token} = require('config.json');
 // Initialize discord api
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 // Add collection of commands to client (bot)
 client.commands = new Discord.Collection();
+
+// Filter commands to only javascript files
+const commandFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 // Runs when bot is ready!
 client.on('ready', () => {
@@ -19,7 +27,23 @@ client.on('ready', () => {
 
 // Listen for messages
 client.on('message', message => {
+  // Return if message is sent by a bot
+  if (message.author.bot) return;
 
+  // Commands
+  if (message.content.startsWith(prefix)) {
+    // Change message to array and use shift to remove command out of arguments.
+    const args = message.content.slice(prefix.lenght).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      message.reply('Huh?');
+    }
+  }
+  
+  // Word detection
 });
 
 // Connect bot to account with token
