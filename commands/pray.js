32984a4d4d5a9ae005b24da8f72be4f8f234c8e4
Etless <askar2_00@hotmail.com>
2020-09-08 23:20:21 +0200

    Fixed written time difference

--- commands/pray.js
+++ commands/pray.js
@@ -1,27 +1,34 @@
 const User   = require('../modules/user/User.js');
 const Random = require('../modules/util/Random.js')
 
+const reward = 10;
+
 module.exports = {
   name: 'pray',
-  description: 'Prayed for forgiveness.',
+  description: 'Pray for forgiveness.',
   guildOnly: true,
   adminOnly: false,
   execute(message, args) {
     const _reply = ["Prayed for forgiveness :pray:","Read the bible :book:"];
 
     var user = new User(message.member);
-    const diff = user.time.diff();
+    var diff = user.time.diff();
+
     // Check if difference between now and then is over 2 hours
     if (diff >= 2) {
-      user.saint.add(10);
+      user.saint.add(reward);
       user.time.now();
       message.channel.send(`${message.author}`+" "+_reply[Random.nextInt(2)]);
+      user.save();
     } else {
       // Calculate time left
+      diff = 2 - diff;
       const hour = Math.floor(diff);
-      const min  = Math.floor((diff % 1) * 60);
-      message.channel.send(`${message.author}`+" You need to wait "+(hour !== 0 ? hour+"h " : "")+ (min !== 0 ? min+"m" : "") +".");
+      const min  = Math.floor(diff % 1 * 60);
+      message.channel.send(`${message.author}`+" You need to wait "+
+        (hour !== 0 ? hour+"h " : "")+ // Write hours
+        (min !== 0 ? min+"m" : "")+    // Write minutes
+        ".");
     }
-    user.save();
   },
 };
