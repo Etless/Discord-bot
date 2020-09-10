9ba2151b9833b6e897bbee698db160e0bdb85e2e
Etless <askar2_00@hotmail.com>
2020-09-11 00:23:12 +0200

    Added ability to check other users rank

--- commands/rank.js
+++ commands/rank.js
@@ -6,6 +6,18 @@ module.exports = {
   guildOnly: true,
   adminOnly: false,
   execute(message, args) {
+
+    if (args[0] === '-user') {
+      // Check if someone has been mentioned in the message
+      const member = message.mentions.members.first();
+      if (member == null)
+        return message.reply('You need to mention a user to see their rank!');
+
+      const user = new User(member);
+      return message.channel.send(`${user.member}`+", "+user.saint.get());
+    }
+
+    // Default response
     const user = new User(message.member);
     message.reply(user.saint.get());
   },
